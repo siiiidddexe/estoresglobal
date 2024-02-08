8836a8b0df354c63bf35612d49c6f8db11273427
Siddhant Sundar <siddhantsundar2016@gmail.com>
2024-02-08 08:57:04 +0530

    V.1.0.6

---
+++ assets/js/whatsapp_api.js
@@ -0,0 +1,11 @@
+function sendToWhatsapp() {
+    let number = "+918549013115";
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElement ById('message') .value;
+  var url = "https://wa.me/" + number + "?text="
+  + "Name : " +name+ "%0a"
+  + "Email : " +email+ "%0a"
+  + "Message : " +message+ "%0a%0a";
+  window.open(url, '_blank').focus();
+}
