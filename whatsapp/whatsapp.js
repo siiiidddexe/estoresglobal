f5ac61dc8fa7f8a8d22bc8545d2982efd50f79d2
Siddhant Sundar <siddhantsundar2016@gmail.com>
2024-02-09 22:51:00 +0530

    V1.0.9
    Forms Working and What's App API Working.

--- whatsapp/whatsapp.js
+++ whatsapp/whatsapp.js
@@ -4,11 +4,17 @@ function sendToWhatsapp(){
 	let name = document.getElementById('name').value;
 	let email = document.getElementById('email').value;
 	let message = document.getElementById('message').value;
-
+	let select = document.getElementById('selectedOption').value;
+	let select2 = document.getElementById('argb').value;
+	let name2 = document.getElementById('name2').value;
+	let topic = document.getElementById('topics').value;
 	var url = "https://wa.me/" + number + "?text="
 	+ "Name : " +name+ "%0a"
 	+ "Email : " +email+ "%0a"
-	+ "Message : " +message+ "%0a%0a";
-
+	+ "College Address : " +message+ "%0a"
+	+ "Service Type: " +select+ "%0a"
+	+ "Timings: " +select2+ "%0a"
+	+ "College Name: " +name2+ "%0a"
+	+ "Topic: " +topic+ "%0a"
 	window.open(url, '_blank').focus();
 }
