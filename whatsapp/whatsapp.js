1cbf336b1065b6a524702671eef3578bc8fda327
Siddhant Sundar <siddhantsundar2016@gmail.com>
2024-04-11 09:25:18 +0530

    updates

--- whatsapp/whatsapp.js
+++ whatsapp/whatsapp.js
@@ -2,8 +2,8 @@ function sendToWhatsapp(){
 	let number = "+917829753178";
 
 	let name = document.getElementById('name').value;
-	let noso = document.getElementById('nos').value;
-	let phoneo = document.getElementById('phone').value;
+	let nos = document.getElementById('noos').value;
+	let phone = document.getElementById('phoone').value;
 	let email = document.getElementById('email').value;
 	let message = document.getElementById('message').value;
 	let select = document.getElementById('selectedOption').value;
@@ -18,7 +18,7 @@ function sendToWhatsapp(){
 	+ "*Date:*" +" "+select2+" "+ "%0a"
 	+ "*College Name:*" +" "+name2+" "+ "%0a"
 	+ "*Departments:*" +" "+topic+" "+ "%0a"
-	+ "*No Of Students*" +" "+noso+" "+ "%0a"
-	+ "*Phone Number*" +" "+phoneo+" "+ "%0a"
+	+ "*Phone Number:*" +" "+phone+" "+ "%0a"
+	+ "*Number of Students:*" +" "+nos+" "+ "%0a"
 	window.open(url, '_blank').focus();
 }
